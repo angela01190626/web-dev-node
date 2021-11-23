8c20d534c167cbbddf8005578544d3adab425d2a
Yangjiawen Xu <xuxx1262@umn.edu>
2021-11-23 16:11:37 +0800

    fix some problem

--- services/profile-service.js
+++ services/profile-service.js
@@ -3,7 +3,7 @@ let profile = require('../data/profile.json');
 module.exports = (app) => {
 
     const getCurrentProfile = (req, res) => {
-        res.json(profile);
+        res.json(profile[0]);
     }
 
     app.get('/api/profile', getCurrentProfile);
